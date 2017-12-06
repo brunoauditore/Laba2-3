edbac0166512b21ed88378579495fdf668292f03
Bogdan <brunoauditore15@gmail.com>
2017-12-06 23:09:27 +0200

    Update Run.java

--- Run.java
+++ Run.java
@@ -62,7 +62,9 @@ public class Run {
     }
 
     private void Print_List() {
-        System.out.println(list);
+        for(int i = 0; i < list.size(); i ++) {
+            System.out.println(list.get(i));
+        }
     }
 
     private void add_stone() {
